2cc3b25560e147a6f2aa305d821caf1986d4f670
louismaillet <louis.maillet@etu.univ-orleans.fr>
2025-03-27 08:53:33 +0100

    ajout fichier junit + classs Livre

---
+++ Livre.java
@@ -0,0 +1,27 @@
+public class Livre {
+    private String isbn;
+    private String titre;
+    private int nbPages;
+    private String datePubli;
+    private double prix;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public int getNbPages() {
+        return nbPages;
+    }
+
+    public String getDatePubli() {
+        return datePubli;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+}
