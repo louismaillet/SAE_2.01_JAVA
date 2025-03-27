06242bfd77af8e1d8f9653adb5414bbbfad60d45
louismaillet <louis.maillet@etu.univ-orleans.fr>
2025-03-27 09:30:05 +0100

    classe test junit

--- sae-java/src/Livre.java
+++ sae-java/src/Livre.java
@@ -5,6 +5,14 @@ public class Livre {
     private String datePubli;
     private double prix;
 
+    public Livre(String isbn, String titre, int nbPages, String datePubli, double prix) {
+        this.isbn = isbn;
+        this.titre = titre;
+        this.nbPages = nbPages;
+        this.datePubli = datePubli;
+        this.prix = prix;
+    }
+
     public String getIsbn() {
         return isbn;
     }
